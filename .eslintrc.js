8e5e6c8ac2b7e067c1c571c65d7efb1124ab6f8a
Shreyas <shreyas.devalapurkar@gov.bc.ca>
2020-07-17 12:39:25 -0700

    remove linting check, need to move to cc later

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     "react/jsx-curly-brace-presence": "off",
     "import/no-extraneous-dependencies": "off",
     "import/prefer-default-export": "off",
+    "react/forbid-prop-types": "off",
     "prettier/prettier": ["error"]
   }
 };
